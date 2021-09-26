47b1ab193514914b35a85a0a680c36ca341f205d
Ibnul Ferdous <ibnul04@gmail.com>
2021-09-26 23:45:24 +0600

    Minor Edit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header>
             <h1 className="uppercase">Mega Projects Monitor</h1>
-            <p>TheBangladesh government has allocated one-fourth of this year's development budget to 10 mega projects notwithstanding their unimpressive implementation rate. By definition, megaprojects are large-scale, complex ventures. These projects usually cost millions of dollars and are transformational in nature impacting millions of people.</p>
+            <p>The Bangladesh government has allocated one-fourth of this year's development budget to 10 mega projects notwithstanding their unimpressive implementation rate. By definition, megaprojects are large-scale, complex ventures. These projects usually cost millions of dollars and are transformational in nature impacting millions of people.</p>
             <div>
                 <h2 className="uppercase">Total Projects: 10</h2>
                 <h2 className="uppercase">Total Estimated Cost: $40.77 Billion</h2>
