6d12b1cb663d612cba98d82c1c304f7c195e9007
Ibnul Ferdous <ibnul04@gmail.com>
2021-09-26 16:42:43 +0600

    Header Completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="container">
+            <h1>Mega Projects Monitor</h1>
+            <div>
+                <h2>Projects: 14</h2>
+                <h2>Total Estimated Cost: $0000 Billion</h2>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
